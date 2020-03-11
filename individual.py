27ed704362ce155ee4980ad201609b148632788c
Kelsea Best <kelsea.b.best@vanderbilt.edu>
2020-03-11 16:55:51 -0500

    working on indv

--- individual.py
+++ individual.py
@@ -14,3 +14,18 @@ import random
 import math
 import numpy as np
 import matplotlib.pyplot as plt
+
+class individual :
+    def __init__(self): #initialize utilities
+        self.age = np.random.normal(44, 12)
+        gend_arr = ['M', 'F']
+        self.gender = np.random.choice(gend_arr, 1)
+        pass
+
+
+    def assign_hh(self, agent_set):
+        pass
+
+
+    def age(self):
+        self.age = self.age + 1
