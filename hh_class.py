d9a3419f34c9852f7577444be8c2cf85cd704153
Kelsea Best <kelsea.b.best@vanderbilt.edu>
2020-05-26 10:45:34 -0500

    cleaning up

--- hh_class.py
+++ hh_class.py
@@ -52,7 +52,7 @@ class Household :
 
 
 #assign individuals to a household
-    def gather_members(self, individual_set)
+    def gather_members(self, individual_set):
         ind_no_hh = individual_set['hh'] == None
         self.individuals.append(ind_no_hh.sample(self.hh_size))
         for p in self.individuals:
@@ -66,12 +66,7 @@ class Household :
             p.owner = self
 
     def check_land(self):
-        my_land = patches['owner'] == self
-        for p in my_land:
-            if p.island == 0:
-                self.land_owned -= 1
-                self.land_impacted = 1
-                self.patches_owned.remove(p)
+        pass
 
     def set_network(self, agent_set, network_structure, network_size):
         if network_structure == 'random':
@@ -85,25 +80,12 @@ class Household :
                 self.network_moves += 1
 
 
-#need to update this
-    def migrate(self, decision, mig_threshold, migrations):
-        if self.employment == 0 and self.land_owned <= 0: #different kinds of mig
-            if decision == 'utility':
-                intercept = -1.3
-                age_factor = 0
-                beta_size = 0
-                beta_network = 0
-                linear = intercept + beta_wealth * self.wealth +
-                                beta_size * (self.nonworkers/ self.household_size)+
-                                beta_network * self.network_moves + age_factor
-
-        #logistic function with prob_migrate
-        self.prob_migrate = (exp(linear) / (1+ exp(linear)))
-
-        if random.random() < self.prob_migrate and self.wealth >= mig_threshold:
-            self.someone_migrated = 1
-            migrations += 1
-            #right now the agent just leaves the model
+    def migrate(self, method):
+        if method == 'utility':
+            utility_max.decide(self.agent_var)
+        else:
+            pass
+#different kinds of mig
 
     #this is where each individual will assess utilty
     def sum_utility(self, individual_set):
@@ -113,22 +95,4 @@ class Household :
 
 
     def update_wealth(self):
-        if self.land_owned > 0:
-            prod = 0
-            for p in self.patches_owned:
-                prod = prod + p.productivity
-            self.wealth = self.wealth - sum(self.payments) + prody
-            self.wtp = self.wtp * (1- (len(self.employees)/10))
-        if self.wealth < 0:
-                self.wealth = 0
-        elif self.employment == 1:
-            self.wealth = self.wealth + self.salary
-            self.wtp = 0
-            if self.wealth < 0:
-                self.wealth = 0
-        elif self.someone_migrated == 0:
-            self.wealth = self.wealth - self.expenses
-            selt.wtp = 0
-            self.wta = self.wta * 0.9
-            if self.wealth < 0:
-                self.wealth = 0
+        pass 
