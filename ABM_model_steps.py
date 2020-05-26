7ac5e60b27bd50c2a860276ed1cde91fb4af2e0f
Kelsea Best <kelsea.b.best@vanderbilt.edu>
2020-05-26 10:30:14 -0500

    working on model May 26

--- ABM_model_steps.py
+++ ABM_model_steps.py
@@ -17,12 +17,12 @@ import pandas as pd
 
 #initialize model
 class ABM_Model:
-    def __init__(self, N_hh, N, decision, mig_threshold):
+    def __init__(self, N_hh, N_ind, decision, mig_threshold):
         self.decision = decision #set decision type
         self.mig_threshold = mig_threshold #wealth threshold to migrate
         #self.network_structure = network_structure
         self.num_hh = N_hh #households
-        self.num_individuals = N
+        self.num_individuals = N_ind
         init_time = 0
         self.tick = init_time
         self.migrations = 0 #Initialize number of overall migrations
@@ -41,33 +41,41 @@ class ABM_Model:
             self.individual_set = pd.concat([self.individual_set, row])
 
         # Create households
-        self.household = pd.DataFrame() #empty list to store agents created
+        self.hh_set = pd.DataFrame() #empty list to store agents created
         for i in range(self.num_hh):
             a = Household()
             a.individuals = a.gather_members(self.individual_set)
-            a.land_owned = a.assign_land(self.patch_list) #assign land ownership
-            row = pd.DataFrame({'agent': [a], 'id': [a.unique_id], 'wtp': [a.wtp],
+            a.land_owned = a.assign_land() #assign land ownership
+            row = pd.DataFrame({'household': [a], 'hh_id': [a.unique_id],
+                                'wtp': [a.wtp],
                                'wta': [a.wta], 'employer': [a.employer]})
-            self.household = pd.concat([self.household, row])
+            self.hh_set = pd.concat([self.hh_set, row])
 
         #for a in self.household['agent']:
             #a.set_network()
 
 
     def model_step(self): #model step does each
-        random_sched = np.random.permutation(self.num_hh)
+        random_sched_hh = np.random.permutation(self.num_hh)
+        random_sched_ind = np.random.permutation(self.num_individuals)
         #random schedule each time
 
         origin_comm.shock()
 
-        for i in random_sched: #these are the steps for each agent to go through
-            #add seed
-            agent_var = self.household[self.household.id == i].agent
+        for j in random_sched_ind: #steps for individuals
+            ind_var = self.individual_set[self.individual_set.id == j].ind
+            ind_var.find_work(self.origin_comm, self.hh_set)
+            ind_var.calc_utility()
+            ind_var.age() #age 1 year
+
+
+        for i in random_sched_hh: #these are the steps at each tick for hh
+            agent_var = self.hh_set[self.hh_set.hh_id == i].household
             agent_var.check_network()
             agent_var.check_land()
-            agent_var.look_for_work()
             agent_var.migrate()
             agent_var.update_wealth()
+            utility_max.decide(self.agent_var)
 
         self.tick += 1
 
--- individual.py
+++ individual.py
@@ -26,14 +26,25 @@ class Individual :
         self.gender = np.random.choice(gend_arr, 1)
         self.hh = None
         self.employment = None
-        self.evaluation_method = evaluation_method #passing evaluation object
         pass
 
     def age(self):
         self.age = self.age + 1
 
-    def set_employment(self):
-        pass
 
-    def evaluate_choices(self, evaluation_method):
+    def find_work(self, community, hh_set):  #should this be individual?
+        poss_employers = []
+        for a in agent_set['agent']:
+            if a.wtp >= self.wta and a.land_owned > 0:
+                poss_employers.append(a)
+            if len(poss_employers) != 0:
+                employer = random.choice(poss_employers)
+                self.employer = employer
+                self.salary = (self.wta + employer.wtp)/2
+                self.employment = 1
+                pay = self.salary
+                employer.employees.append(self)
+                employer.payments.append(pay)
+
+    def calc_utility(self):
         pass
