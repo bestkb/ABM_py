ad8155daa50bde69b7a0589c45a9b2c6fce28de8
Kelsea Best <kelsea.b.best@vanderbilt.edu>
2020-06-25 15:13:19 -0500

    thinking about MC

--- monte_carlo.py
+++ monte_carlo.py
@@ -19,6 +19,7 @@ N_hh = 10 #number of households
 run_time = 100 #steps to run
 decision = "utility"
 
+#current values
 mig_threshold = 1000 #migration threshold
 mig_util = 400 #utility to migrate
 wealth_factor = 3000
@@ -27,32 +28,35 @@ ag_factor = 100
 #these are for monte carlo
 size_mc = 1000 #number of MC draws
 
-mig_threshold_mean = 1000
-mig_threshold_sd = 500
-mig_threshold_mc = np.random.normal(mig_threshold_mean, mig_threshold_sd, size_mc)
+mig_threshold_sd = 500 #standard deviation
+mig_threshold_mc = np.random.normal(mig_threshold, mig_threshold_sd, size_mc)
 
-mig_util_mean = 400
-mig_util_sd = 200
-mig_util_mc = np.random.normal(mig_util_mean, mig_util_sd, size_mc)
+mig_util_sd = 200 #standard deviation
+mig_util_mc = np.random.normal(mig_util, mig_util_sd, size_mc)
 
-wealth_mean = 3000
-wealth_sd = 1000
-wealth_factor_mc = np.random.normal(wealth_mean, wealth_sd, size_mc)
+wealth_sd = 1000 #standard deviation
+wealth_factor_mc = np.random.normal(wealth_factor, wealth_sd, size_mc)
 
-
-ag_mean = 100
-ag_sd = 20
-ag_factor_mc = np.random.normal(ag_mean, ag_sd, size_mc)
+ag_sd = 20 #standard deviation
+ag_factor_mc = np.random.normal(ag_factor, ag_sd, size_mc)
 
 
-#here I can do a for loop to loop through MC 
+#here I can do a for loop to loop through MC
 # initialize model
-model = ABM_Model(run_time, N_hh, N, decision, mig_util, mig_threshold, wealth_factor, ag_factor)
 
-#run model for ticks
-while (model.tick < model.ticks):
-    model.model_step()
-    model.data_collect()
-    model.tick_up()
-data = model.data_set  #data should be stored here
-migs = model.migrations
+mig_mc = pd.DataFrame()
+
+for i in mig_threshold_mc:
+    model = ABM_Model(run_time, N_hh, N, decision, mig_util, i, wealth_factor, ag_factor)
+
+    #run model for ticks
+    while (model.tick < model.ticks):
+        model.model_step()
+        model.data_collect()
+        model.tick_up()
+    data = model.data_set  #data should be stored here
+    migs = model.migrations
+    
+    final_migs =
+    row = pd.DataFrame({'mig_threshold': [i], 'migrations': [final_migs]})
+    mig_mc = pd.concat([mig_mc, row])
