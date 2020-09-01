922925d027b9aa4d232f9ee62c58a169b630996d
kelsea <kelsea.b.best@vanderbilt.edu>
2020-09-01 07:19:31 -0500

    tracking hh's

--- track_hh_vis.R
+++ track_hh_vis.R
@@ -9,15 +9,26 @@ names(mc_runs) = "mc_run"
 
 hh_track = hh_track %>% cbind(mc_runs) 
  
-hh_track <- hh_track %>% 
- mutate(mig_binary = ifelse(migrations > 0, 1, 0))
+hh_mig <- hh_track %>% 
+  filter(tick == 19) %>%
+ mutate(mig_binary = ifelse(migrations > 0, 1, 0)) %>%
+  select(hh_id, mig_binary, mc_run)
 
-hh_track_1 = hh_track %>%
-  filter(mc_run == 1)
+hh_track <- hh_track %>%
+  inner_join(hh_mig, by = c("hh_id", "mc_run"))
 
-hh_track_1 %>% 
+hh_means <- hh_track %>% group_by(mig_binary, tick) %>%
+  summarise(mean_mig = mean(migrations), sd_mig = sd(migrations),
+            mean_wealth = mean(wealth), sd_wealth = sd(wealth))
+
+hh_means %>% 
   ggplot()+
-  geom_line(aes(x = tick, y = wealth, group= hh_id, color = as.factor(mig_binary)), alpha = 0.7)+
+  geom_line(aes(x = tick, y = mean_wealth, group = mig_binary,    
+                color = as.factor(mig_binary)), size = 1.4)+
+  geom_line(aes(x = tick, y = mean_wealth + sd_wealth, group = mig_binary,    
+                color = as.factor(mig_binary)), linetype = "dashed")+
+  geom_line(aes(x = tick, y = mean_wealth - sd_wealth, group = mig_binary,    
+                color = as.factor(mig_binary)), linetype = "dashed")+
   theme_bw()+
   labs(x = "Tick", y = "HH Wealth")
   
