0a76edc50d625ec6a8a1cc6e6a92a3c7faa1252d
Kelsea Best <kelsea.b.best@vanderbilt.edu>
2020-09-30 17:37:15 -0500

    looking at lazer more

--- Lazar_data.R
+++ Lazar_data.R
@@ -10,8 +10,27 @@ lazar_data_longer <- lazar_data %>% pivot_longer(cols = c("1991", "1995",
                                                              "2010", "2014")) %>%
   na.locf()
 names(lazar_data_longer) = c("type", "level", "year", "value")
+expenditures <- lazar_data_longer
 
 
+lazar_income<- read_xlsx("Lazar_data.xlsx", sheet = "Observed incomes - 2014", skip = 2)%>% 
+  select(c(1:13)) %>%
+  gather(type, income, -c(`Hhold-type ID`, Season))
+
+lazar_income$type <- sub("\\..*", "",lazar_income$type)
+
+
+lazar_expenditures <-read_xlsx("Lazar_data.xlsx", sheet = "Observed incomes - 2014", skip = 2)%>% 
+  select(c(1, 2, 14:21)) %>%
+  gather(type, expenditures, -c(`Hhold-type ID`, Season)) 
+lazar_expenditures$type <- sub("\\..*", "",lazar_expenditures$type)
+
+lazar_input <- lazar_income %>% 
+  inner_join(lazar_expenditures)
+
+lazar_onehh <- lazar_input %>% 
+  filter(`Hhold-type ID` == "AR1(LL)" | `Hhold-type ID` == "AR1(SLO)" |
+           `Hhold-type ID` == "AR1(LLO)")
 
 lazar_data_figs <- read_xlsx("Lazar_fig_data.xlsx", sheet = "Fig2 & Fig5 & FigS2.1", skip =1) %>%
   pivot_longer(cols = c(-"ArchetypeID", -"Livelihood type")) %>% 
