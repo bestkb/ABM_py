8d2508e98478d1283ab9af4ade8b13be4291d353
Kelsea Best <kelsea.b.best@vanderbilt.edu>
2020-11-21 12:17:19 -0600

    finer scale comm impact test

--- current_model_code/comm_impact_test.py
+++ current_model_code/comm_impact_test.py
@@ -17,34 +17,41 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #define vars
+list = [0, 0.05, 0.1, 0.15, 0.02, 0.25, 0.4]
+
 N = 700 #number of individual agents
 N_hh = 100 #number of households
-mig_threshold = 1000 #migration threshold
-mig_util = 400 #utility to migrate
-ag_factor = 100 #land productivity factor
-wealth_factor = 3000 #initialization of wealth factor
+mig_threshold = 2000000 #migration threshold
+mig_util = 5000 #utility to migrate
+ag_factor = 10000 #land productivity factor
+wealth_factor = 500000 #initialization of wealth factor
 run_time = 20 #steps to run
 decision = "utility"
-comm_scale = 0.6
-mc_runs = 50 #number of runs in MC 
+mc_runs = 20 #number of runs in MC 
 
-data = pd.DataFrame()
 data2 = pd.DataFrame()
-for i in range(mc_runs):
-    # initialize model
-    model = ABM_Model(run_time, N_hh, N, decision, mig_util, mig_threshold, wealth_factor, ag_factor, comm_scale)
-    #run model for ticks
-    while (model.tick < model.ticks):
-        model.model_step()
-        model.data_collect()
-        model.tick_up()
 
-    last_data = model.last 
-    final_migs = model.migrations.tail(1)  #data should be stored here
-    data = pd.concat([data, last_data])
-    data2 = pd.concat([data2, final_migs])
-    print(i)
-    Household.next_uid = 1
-    Individual.next_uid = 1
+for j in list: 
+    comm_scale =  j  
 
-data.to_csv("comm_freq_test08.csv")
+    #now run through MC loop 
+    data = pd.DataFrame()
+    data2 = pd.DataFrame()
+    for i in range(mc_runs):
+        # initialize model
+        model = ABM_Model(run_time, N_hh, N, decision, mig_util, mig_threshold, wealth_factor, ag_factor, comm_scale)
+         #run model for ticks
+        while (model.tick < model.ticks):
+            model.model_step()
+            model.data_collect()
+            model.tick_up()
+            
+        last_data = model.last 
+        
+        data = pd.concat([data, last_data])
+        Household.next_uid = 1
+        Individual.next_uid = 1
+    
+    string_1 = "impact_finescale_Nov2020_" + str(j) +  ".csv"
+
+    data.to_csv(string_1)
