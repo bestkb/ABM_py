e608657210fd14f91313c999f2ddcd62ed0865ab
kelsea <kelsea.b.best@vanderbilt.edu>
2020-12-22 13:56:37 +0000

    looked at job sens results... doesn't seem to have a big difference

--- experiment_code/jobs_sens_dec2020.R
+++ experiment_code/jobs_sens_dec2020.R
@@ -1,15 +1,63 @@
 library(tidyverse)
 
-myfiles = list.files(path = "current_model_code/", pattern = "*.csv")
+myfiles = list.files(path = ".", pattern = "*.csv")
 
 #set current_model_code as WD
 all = c()
 for (i in myfiles){
-  jobs <- str_split(i, "_")[[1]][3]
+  jobs <- str_split(i, "_")[[1]][4]
   jobs = gsub("^[^_]*_|\\.csv", "", jobs)
-  jobs = substr(jobs, )
+  jobs = as.numeric(jobs)
   data = read.csv(i)
   data = as.data.frame(data)
-  data = data %>% mutate(run_number = model, comm_impact_scale = comm_scale) 
+  data = data %>% mutate(num_jobs = jobs) 
   all = bind_rows(all, data)
 }
+
+all <- all %>% 
+  mutate(mig_binary = ifelse(migrations > 0, 1, 0))
+
+summary <- all %>% group_by(hh_id, mig_util, mig_threshold, comm_scale, num_jobs) %>%
+  summarise(av_migs = mean(migrations),
+            sd_migs = sd(migrations),
+            av_wealth = mean(wealth),
+            sd_wealth = sd(wealth)) 
+
+summary_diff = all %>%
+  group_by(hh_id, mig_util, mig_threshold, comm_scale, mig_binary, num_jobs) %>%
+  summarise(av_wealth = mean(wealth),
+            sd_wealth = sd(wealth),
+            av_impact = mean(num_shocked))
+
+
+
+summary %>% 
+  ggplot()+
+  geom_boxplot(aes(x= as.factor(comm_scale), y = av_migs))+
+  labs(x = "Community Impact Factor", y = "Average Migrations/ HH")+
+  facet_wrap(~num_jobs)+ 
+  theme_bw()
+
+
+summary %>% 
+  ggplot()+
+  geom_boxplot(aes(x= as.factor(comm_scale), y = av_wealth, fill = as.factor(num_jobs)))+
+  labs(x = "Community Impact Factor", y = "Average HH Wealth")+
+  theme_bw()
+
+
+summary_diff %>% 
+  ggplot()+
+  geom_boxplot(aes(x= as.factor(comm_scale), fill = as.factor(mig_binary), y = av_wealth))+
+  labs(x = "Community Impact Factor", y = "Average HH Wealth")+
+  facet_wrap(~num_jobs)+ 
+  theme_bw()
+
+
+summary_diff %>% 
+  ggplot()+
+  geom_boxplot(aes(x= as.factor(comm_scale), fill = as.factor(mig_binary), y = av_impact))+
+  labs(x = "Community Impact Factor", y = "Average HH Impact")+
+  facet_wrap(~num_jobs)+ 
+  theme_bw()
+
